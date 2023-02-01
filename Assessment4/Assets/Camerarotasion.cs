070919065c523c63a2fa8a791f466682de0fa4d0
Vu <doragan8@gmail.com>
2023-02-01 17:17:02 +0700

    finish assess 4

--- Assessment4/Assets/Camerarotasion.cs
+++ Assessment4/Assets/Camerarotasion.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Camerarotasion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float direction = 1f;
+    public float speed = 5f;
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            float angle = transform.eulerAngles.y;
+            if (angle > 180f) angle -= 360f;
+            if ((angle < -100f) || (angle > 100f)) direction *= -1f;
+            transform.eulerAngles += 5 * direction * new Vector3(0, -Input.GetAxis("Mouse X"), 0);
+        }
     }
 }
